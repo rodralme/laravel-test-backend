4ea7c9468f0952445866d654fce4de3ecf082450
Rodrigo <rodralme@gmail.com>
2020-10-20 22:22:32 -0300

    construção do formulário de cadastro de imóveis

---
+++ resources/js/components/InputText.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="flex items-center">
+        <label :for="name" class="w-64 inline-block">
+            {{ label }}
+        </label>
+        <input
+            :id="name"
+            :name="name"
+            type="text"
+            class="px-3 py-2 border rounded-md flex-1"
+        />
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'input-text',
+
+        props: ['name', 'label'],
+    }
+</script>
--- resources/js/views/imoveis/Form.vue
+++ resources/js/views/imoveis/Form.vue
@@ -1,5 +1,43 @@
 <template>
-    <div>
-        Form
+    <div class="text-gray-700">
+        <h2 class="w-full text-xl font-bold pb-6">
+            Cadastro de Imóvel
+        </h2>
+        <form>
+            <ul class="border rounded-md">
+                <li class="px-4 py-4 border-b">
+                    <input-text name="email_proprietario" label="E-mail do proprietário"/>
+                </li>
+                <li class="px-4 py-4 border-b">
+                    <input-text name="rua" label="Rua"/>
+                </li>
+                <li class="px-4 py-4 border-b">
+                    <input-text name="numero" label="Número"/>
+                </li>
+                <li class="px-4 py-4 border-b">
+                    <input-text name="complemento" label="Complemento"/>
+                </li>
+                <li class="px-4 py-4 border-b">
+                    <input-text name="cidade" label="Cidade"/>
+                </li>
+                <li class="px-4 py-4 border-b">
+                    <input-text name="estado" label="Estado"/>
+                </li>
+            </ul>
+        </form>
     </div>
 </template>
+
+<script>
+import InputText from "../../components/InputText"
+
+export default {
+    components: {InputText}
+}
+</script>
+
+<style>
+    form > ul > li:last-child {
+        border: none;
+    }
+</style>
