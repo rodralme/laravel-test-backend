d512bffa9a09290a73167d6f8d3f8ea75b24f5c3
Rodrigo <rodralme@gmail.com>
2020-10-21 23:59:50 -0300

    validação de CPF com teste unitário

---
+++ app/Rules/CPFValidoRule.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+
+class CPFValidoRule implements Rule
+{
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param string $attribute
+     * @param mixed $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        // Verifica se um número foi informado
+        if (is_null($value) || $value == '') {
+            // deixa a validação passar pois pode ser um campo não obrigatório
+            return true;
+        }
+
+        // Elimina possivel mascara
+        $cpf = preg_replace("/[^0-9]/", "", $value);
+        $cpf = str_pad($cpf, 11, '0', STR_PAD_LEFT);
+
+        // Verifica se o numero de digitos informados é igual a 11
+        if (strlen($cpf) != 11) {
+            return false;
+        }
+
+        // Verifica se foi informada uma sequência de digitos repetidos. Ex: 111.111.111-11
+        if (preg_match('/(\d)\1{10}/', $cpf)) {
+            return false;
+        }
+
+        // Faz o cálculo para validar o CPF
+        for ($t = 9; $t < 11; $t++) {
+            for ($d = 0, $c = 0; $c < $t; $c++) {
+                $d += $cpf[$c] * (($t + 1) - $c);
+            }
+            $d = ((10 * $d) % 11) % 10;
+            if ($cpf[$c] != $d) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'CPF inválido.';
+    }
+}
---
+++ tests/Unit/TestCPFRule.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Rules\CPFValidoRule;
+use PHPUnit\Framework\TestCase;
+
+class TestCPFRule extends TestCase
+{
+    /**
+     * @var CPFValidoRule
+     */
+    private CPFValidoRule $rule;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->rule = new CPFValidoRule();
+    }
+
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testDeveAceitarCPFComValorVazio()
+    {
+        $this->assertTrue($this->rule->passes('attr', ''));
+    }
+
+    /**
+     * @dataProvider cpfValidos
+     * @param string $cpf
+     * @return void
+     */
+    public function testDeveAceitarCPFValido($cpf)
+    {
+        $this->assertTrue($this->rule->passes('attr', $cpf));
+    }
+
+    /**
+     * @dataProvider cpfInvalidos
+     * @param string $cpf
+     * @return void
+     */
+    public function testNaoPodeAceitarCPFInvalido($cpf)
+    {
+        $this->assertFalse($this->rule->passes('attr', $cpf));
+    }
+
+    public function cpfValidos()
+    {
+        return [
+            ['927.755.439-87'],
+            ['339.431.414-20'],
+            ['33661241800'],
+            ['57734018351'],
+        ];
+    }
+
+    public function cpfInvalidos()
+    {
+        return [
+            ['0'],
+            ['339.431.414-2'],
+            ['927.755.439-88'],
+            ['57734018352'],
+            ['11111111111'],
+            ['88888888888'],
+        ];
+    }
+}
